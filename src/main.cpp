60b2f29b048578bb99dfa045b0264c949a04b8be
Chanhwi Lee <chanhwi.lee6@gmail.com>
2022-03-26 13:30:19 +0900

    Practice 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include <DHTesp.h>
+#include <SSD1306.h>
 
 DHTesp              dht;
+SSD1306             display(0x3c, 4, 5, GEOMETRY_128_32);
 int                 interval = 2000;
 unsigned long       lastDHTReadMillis = 0;
 float               humidity = 0;
@@ -11,8 +13,8 @@ void setup() {
     Serial.begin(115200);
     dht.setup(14, DHTesp::DHT22); // Connect DHT sensor to GPIO 14
     delay(1000);
-
     Serial.println(); Serial.println("Humidity (%)\tTemperature (C)");
+    
 }
 
 void readDHT22() {
@@ -20,7 +22,6 @@ void readDHT22() {
 
     if(currentMillis - lastDHTReadMillis >= interval) {
         lastDHTReadMillis = currentMillis;
-
         humidity = dht.getHumidity();              // Read humidity (percent)
         temperature = dht.getTemperature();        // Read temperature as Fahrenheit
     }
@@ -28,5 +29,12 @@ void readDHT22() {
 void loop() {
     readDHT22();
     Serial.printf("%.1f\t %.1f\n", temperature, humidity);
+    display.init();
+    display.flipScreenVertically();
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(0, 0, String(temperature));
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(0, 16, String(humidity));
+    display.display();
     delay(1000);
 }
